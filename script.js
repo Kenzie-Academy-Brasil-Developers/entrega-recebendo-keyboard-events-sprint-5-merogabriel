c298fa53ebef9c093a706049bd56b45c03eb578d
Gabriel Mero <og.gabrielmero@gmail.com>
2021-09-30 16:42:24 -0300

    Submit

---
+++ script.js
@@ -0,0 +1,19 @@
+let boxTop = 200
+let boxLeft = 200
+
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key
+    console.log('keydown event\n\n' + 'key: ' + keyName)
+    if (keyName === 'ArrowDown') {
+        boxTop += 10
+    } else if (keyName === 'ArrowUp') {
+        boxTop -= 10
+    } else if (keyName === 'ArrowLeft') {
+        boxLeft -= 10
+    } else if (keyName === 'ArrowRight') {
+        boxLeft += 10
+    }
+    console.log(boxTop, boxLeft)
+    document.getElementById("box").style.top = boxTop + "px"
+    document.getElementById("box").style.left = boxLeft + "px"
+})
